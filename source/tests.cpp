fbae81676332832757bc3d1b97e9b742959721ad
Kai Lorenz <kai.lorenz@uni-weimar.de>
2020-05-26 12:12:20 +0200

    2.1  and 2.2

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,5 +1,19 @@
 #define CATCH_CONFIG_RUNNER
+#include "vec2.hpp"
 #include <catch.hpp>
+
+
+
+TEST_CASE("test_vec_init", "[vec2]")
+{
+  Vec2 a{};
+  REQUIRE(a.x == Approx(0.0f));
+  REQUIRE(a.y == Approx(0.0f));
+  Vec2 b {5.1f, -9.3f};
+  REQUIRE(b.x == Approx(5.1f));
+  REQUIRE(b.y == Approx(-9.3f));
+
+}
 
 int main(int argc, char *argv[])
 {
